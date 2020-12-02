d73b6ac2511a2d3e6e551149df5cb99d0e87e3b6
Zessay <zhushuai0403@163.com>
2020-12-02 17:01:28 +0800

    fix dataste cache name

--- lccc/run.sh
+++ lccc/run.sh
@@ -2,5 +2,5 @@ python train.py \
   --pretrained \
   --model_checkpoint=../../../models/LCCC_GPT_base \
   --data_path=../../../corpus/toy/toy_data.json \
-  --dataset_cache=../../../corpus/toy/ \
+  --dataset_cache=../../../corpus/toy/dataset_cache \
   --scheduler=linear
