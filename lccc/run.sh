c2f192568cca7ca15c6b735dc99f84bca4de0fd8
Zessay <zhushuai0403@163.com>
2020-12-02 16:57:17 +0800

    fix model path

--- lccc/run.sh
+++ lccc/run.sh
@@ -1,5 +1,6 @@
 python train.py \
   --pretrained \
-  --model_checkpoint=../../../models/ \
+  --model_checkpoint=../../../models/LCCC_GPT_base \
   --data_path=../../../corpus/toy/toy_data.json \
+  --dataset_cache=../../../corpus/toy/ \
   --scheduler=linear
